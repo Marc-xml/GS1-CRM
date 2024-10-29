97d6b6604ccd547bed4043b0319ba9c96c488777
Marc-xml <118174429+Marc-xml@users.noreply.github.com>
2024-10-29 14:54:55 +0100

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,7 +1,4 @@
-export const API_BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
-    ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://localhost:8888/api/';
+export const API_BASE_URL = "https://gs1crm.onrender.com/api/"
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
